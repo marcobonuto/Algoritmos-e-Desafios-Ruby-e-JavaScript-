e6ce2404bb77cd62b98cb92313a693a7480ba8e4
Marco Bonuto <marcobonuto@yahoo.com>
2015-10-26 17:11:38 -0200

    Ajustes gerais

--- First Reverse/first-reverse.rb
+++ First Reverse/first-reverse.rb
@@ -1,10 +1,10 @@
-def FirstReverse(str)  
+def first_reverse(str)  
   return str.split('').reverse().join('');       
 end
    
-puts FirstReverse("Ruby")
-puts FirstReverse("JavaScript") 
-puts FirstReverse("Chame a string que voce quizer!")      
+puts first_reverse("Ruby")
+puts first_reverse("JavaScript") 
+puts first_reverse("Chame a string que voce quizer!")      
 
 # Primeiro usamos o método split para dividir a seqüência de strings, criando assim um array de caracteres, por exemplo, ['r','u','b', 'y']
 # Depois usamos o métodos .reverse no array criado
