ace155d6baa20bcb91c6ca5373bd0fd52cec6f4f
Marco Bonuto <marcobonuto@yahoo.com>
2015-10-26 17:01:23 -0200

    Comentando o código em ruby

--- First Reverse/first-reverse.rb
+++ First Reverse/first-reverse.rb
@@ -8,3 +8,4 @@ puts FirstReverse("Chame a string que voce quizer!")
 
 # Primeiro usamos o método split para dividir a seqüência de strings, criando assim um array de caracteres, por exemplo, ['r','u','b', 'y']
 # Depois usamos o métodos .reverse no array criado
+# Por mim usamos o método join para juntar novamente e formar a nova string, agora invetida!ssss
