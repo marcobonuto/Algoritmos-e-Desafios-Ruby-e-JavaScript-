a02e94d0d92a16ced0398ad8519638b47d6605f4
Marco Bonuto <marcobonuto@yahoo.com>
2015-10-26 16:52:14 -0200

    Resolvendo exercicio em Ruby

---
+++ First Reverse/first-reverse.rb
@@ -0,0 +1,9 @@
+#
+
+def FirstReverse(str)  
+  return str.split('').reverse().join('');       
+end
+   
+puts FirstReverse("Ruby")
+puts FirstReverse("JavaScript") 
+puts FirstReverse("Chame a string que voce quizer!")      
