02ad8cd3106d4d22e261e86344b346fcefd4bfb9
Marco Bonuto <marcobonuto@yahoo.com>
2015-10-27 15:23:51 -0200

    Ajustes na resoluçao em Ruby

--- Maior Palavra de uma Frase/maior-palavra-da-frase.rb
+++ Maior Palavra de uma Frase/maior-palavra-da-frase.rb
@@ -1,17 +1,16 @@
 def LongestWord(sen)
+	longest = "" # Cria uma variável vazia
 
-  # code goes here
- longest = ""
-
-  sen.scan(/\w+/) do |word|
-    if word.length > longest.length
-      longest = word
-    end
-  end
+	sen.scan(/\w+/) do |word| # O método scan quebra a frase palavra por palavra e depois percorre 
+		if word.length > longest.length 
+			longest = word
+		end
+	end
                       
-  return longest
+	return longest
 end
    
 # keep this function call here 
-# to see how to enter arguments in Ruby scroll down   
-puts LongestWord("Testando a maior string") 
+# to see how to enter arguments in Ruby scroll down  
+
+puts LongestWord("Compare palavra por palavra e me retorna maior delas!")
